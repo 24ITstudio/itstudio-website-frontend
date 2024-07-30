972a0f0df2204148aa083f216630e0eaaf7a083d
yyaojiayu <yaojiayu@stu.ouc.edu.cn>
2024-07-30 14:59:10 +0800

    2

--- src/components/log-query.vue
+++ src/components/log-query.vue
@@ -11,24 +11,34 @@
     </div>
     <div class="query">
       <div class="text">
-        <p>请输入手机号/邮箱/学号进行查询</p>
+        <p>请输入邮箱进行查询</p>
       </div>
       <div class="input">
         <div class="input1">
-          <input type="text" v-model="phoneNumber" placeholder="请输入号码" placeholder-class="place" />
-          <a href=""><img src="../assets/Group.png" alt="" title="点击发送" :disabled="isSending"
-              @click="sendVerificationCode" /></a>
+          <input
+            type="text"
+            v-model="phoneNumber"
+            placeholder="请输入邮箱地址"
+            placeholder-class="place"
+          />
+          <button>发送验证码</button>
         </div>
         <div class="input2">
-          <input type="text" placeholder="请输入验证码" placeholder-class="place" />
-          <a href=""><img src="../assets/Group.png" alt="" title="点击发送" /></a>
+          <input
+            type="text"
+            placeholder="请输入验证码"
+            placeholder-class="place"
+          />
+          <a href=""
+            ><img src="../assets/Group.png" alt="" title="点击查询"
+          /></a>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import navHead from "./nav-head.vue"
+import navHead from "./nav-head.vue";
 export default {
   components: {
     navHead,
@@ -65,7 +75,6 @@ export default {
 a {
   text-decoration: none;
 }
-
 body,
 .box {
   position: relative;
@@ -73,6 +82,7 @@ body,
   height: 100vh;
   background: url(../assets/bg.jpg);
   background-size: 100%;
+  overflow: hidden;
 }
 
 .shadow {
@@ -83,11 +93,6 @@ body,
   background-color: #04132dcc;
   transform: translateY(70px);
 }
-
-.query:hover {
-  background-color: #29447385;
-}
-
 .back {
   position: relative;
   margin: 10px;
@@ -103,20 +108,12 @@ body,
   color: #808da5;
   font-size: 15px;
 }
-
-.back a {
-  text-decoration: none;
-}
-
 .query {
-  transition: all 0.4s;
   position: absolute;
-  left: 31%;
+  left: 33%;
   top: 26%;
-  width: 37%;
+  width: 33%;
   border-radius: 17px;
-  max-width: 100%;
-  overflow: hidden;
   height: 31%;
   transform: translateY(70px);
 }
@@ -129,41 +126,44 @@ body,
   font-family: "Microsoft New Tai Lue";
   color: #fff;
 }
-
 .input {
-  width: 82%;
+  width: 100%;
   height: 100%;
   margin: 0 auto;
 }
-
 .input1,
 .input2 {
   position: relative;
   margin-top: 5px;
   margin-left: 12%;
-  width: 78%;
+  width: 80%;
   height: 23%;
   margin-bottom: 10px;
 }
-
-.input1 img,
 .input2 img {
   position: absolute;
   right: 2%;
   top: 13%;
   width: 40px;
 }
-
-@keyframes img {
-  to {
-    transform: scale(1.2);
-  }
+button {
+  position: absolute;
+  right: 5%;
+  top: 25%;
+  width: 25%;
+  height: 50%;
+  background-color: #29447385;
+  border-radius: 37px;
+  border: none;
+  color: wheat;
+  cursor: pointer;
+  font-size: 14px;
+  background-color: #1c3869;
 }
-
 input {
   letter-spacing: 1.5px;
   color: white;
-  font-size: 18px;
+  font-size: 15px;
   padding-left: 18px;
   width: 100%;
   height: 100%;
@@ -177,5 +177,52 @@ input {
 input::-webkit-input-placeholder {
   font-size: 16px;
   color: #808da5;
+}
+.query {
+  animation-duration: 1s;
+  animation-name: slidein;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+}
+@keyframes slidein {
+  from {
+    transform: translateY(-134px);
+  }
+  to {
+    top: 23%;
+    background-color: #29447385;
+  }
+}
+@media (max-width: 376px) {
+  .query {
+    width: 80%;
+    left: 11%;
+  }
+  .shadow {
+    width: 100%;
+    height: 100%;
+  }
+  .text p {
+    font-size: 16px;
+  }
+  input::-webkit-input-placeholder {
+    font-size: 14px;
+  }
+  input {
+    font-size: 12px;
+  }
+  .input1,
+  .input2 {
+    width: 88%;
+    right: 5%;
+  }
+  button {
+    font-size: 12px;
+    width: 30%;
+    right: 2%;
+  }
+  .input2 img {
+    top: 8%;
+  }
 }
 </style>
