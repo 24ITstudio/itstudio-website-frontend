cc637aef011af8990f83a557746a240c0dacfa85
echoo0519 <2518711819@qq.com>
2024-08-09 23:41:40 +0800

    update

--- src/components/newProgressBar.vue
+++ src/components/newProgressBar.vue
@@ -264,4 +264,195 @@ body,
         opacity: 1;
     }
 }
+
+@media (max-width: 430px) {
+    a {
+        text-decoration: none;
+    }
+
+    body,
+    .box {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        background: url(../assets/bg.jpg);
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .shadow {
+        position: absolute;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: #04132dcc;
+    }
+
+    .back {
+        position: relative;
+        margin-top: 20px;
+        margin-left: 20px;
+    }
+
+    .back img {
+        position: absolute;
+        width: 15px;
+    }
+
+    .back p {
+        padding-left: 20px;
+        color: #808da5;
+        font-size: 12px;
+    }
+
+    .back a {
+        text-decoration: none;
+    }
+
+    .main {
+        position: absolute;
+        width: 85%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 10% 5%;
+    }
+
+    .top {
+        position: relative;
+        width: 100%;
+        justify-content: space-between;
+        align-items: flex-end;
+        display: inline-flex;
+        flex-wrap: wrap;
+    }
+
+    .words {
+        width: 30px;
+        color: gray;
+        font-size: 24px;
+        font-family: Microsoft JhengHei UI;
+        font-weight: 400;
+        word-wrap: break-word;
+        padding-bottom: 20px;
+        padding-top: 20px;
+        text-align: center;
+    }
+
+    .lightUp {
+        color: #fff;
+        font-weight: 700;
+    }
+
+    .lightDown {
+        color: rgb(169, 3, 3);
+        font-weight: 700;
+    }
+
+    .highlight {
+        position: absolute;
+        left: 0;
+        height: 100%;
+        width: 16%;
+        background-color: #fff;
+        border-radius: 10px;
+    }
+
+    .common {
+        height: 100%;
+        width: 100%;
+        background-color: rgba(152, 152, 152, 0.3);
+        border-radius: 10px;
+    }
+
+    .bottom {
+        position: relative;
+        width: 100%;
+        justify-content: space-between;
+        align-items: flex-start;
+        display: inline-flex;
+        flex-wrap: wrap;
+    }
+
+    .mid {
+        height: 20px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+    }
+
+    .scale-in-hor-left {
+        -webkit-animation: scale-in-hor-left 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+        animation: scale-in-hor-left 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    }
+
+    @-webkit-keyframes scale-in-hor-left {
+        0% {
+            -webkit-transform: scaleX(0);
+            transform: scaleX(0);
+            -webkit-transform-origin: 0% 0%;
+            transform-origin: 0% 0%;
+            opacity: 1;
+        }
+
+        100% {
+            -webkit-transform: scaleX(1);
+            transform: scaleX(1);
+            -webkit-transform-origin: 0% 0%;
+            transform-origin: 0% 0%;
+            opacity: 1;
+        }
+    }
+
+    @keyframes scale-in-hor-left {
+        0% {
+            -webkit-transform: scaleX(0);
+            transform: scaleX(0);
+            -webkit-transform-origin: 0% 0%;
+            transform-origin: 0% 0%;
+            opacity: 1;
+        }
+
+        100% {
+            -webkit-transform: scaleX(1);
+            transform: scaleX(1);
+            -webkit-transform-origin: 0% 0%;
+            transform-origin: 0% 0%;
+            opacity: 1;
+        }
+    }
+
+    @media (max-width: 430px) {
+        .words {
+            width: 25px;
+            font-size: 18px;
+            padding-bottom: 15px;
+            padding-top: 15px;
+        }
+
+        .back img {
+            width: 12px;
+        }
+
+        .back p {
+            font-size: 10px;
+        }
+
+        .highlight {
+            border-radius: 8px;
+        }
+
+        .common {
+            border-radius: 8px;
+        }
+
+        .mid {
+            height: 15px;
+        }
+    }
+}
 </style>
